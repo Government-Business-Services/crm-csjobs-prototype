75bd23619b0f189fc0bff9d59cb22f4c36b3c3da
Brian Coult <brian.coult@co-co074472.ad.cabinetoffice.gov.uk>
2023-10-12 11:35:58 +0100

    change to folder structure

--- app/routes.js
+++ app/routes.js
@@ -9,36 +9,37 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here
 
 
+// Budget Approval
+router.post('/field-of-dreams/self-serve/manual-recruitment-request/vacancy-details/budget-approval', function(request, response) {
 
-router.post('/voldemort/self-serve/manual-recruitment-request/vacancy-details/budget-approval', function(request, response) {
-
-    var liveInUK = request.session.data['mmrVacancyBudget']
-    if (liveInUK == "Yes"){
-        response.redirect("/voldemort/self-serve/manual-recruitment-request/vacancy-details/location")
+    var mmrVacancyBudget = request.session.data['mmrVacancyBudget']
+    if (mmrVacancyBudget == "Yes"){
+        response.redirect("/field-of-dreams/self-serve/manual-recruitment-request/vacancy-details/location")
     } else {
-        response.redirect("/voldemort/self-serve/manual-recruitment-request/vacancy-details/request-not-complete")
+        response.redirect("/field-of-dreams/self-serve/manual-recruitment-request/vacancy-details/request-not-complete")
     }
 })
 
 
+// Permanent Role
+router.post('/field-of-dreams/self-serve/manual-recruitment-request/vacancy-details/permanent-role', function(request, response) {
 
-router.post('/voldemort/self-serve/manual-recruitment-request/vacancy-details/permanent-role', function(request, response) {
-
-    var liveInUK = request.session.data['mmrVacancyPermanent']
-    if (liveInUK == "Yes"){
-        response.redirect("/voldemort/self-serve/manual-recruitment-request/vacancy-details/grade")
+    var mmrVacancyPermanent = request.session.data['mmrVacancyPermanent']
+    if (mmrVacancyPermanent == "Yes"){
+        response.redirect("/field-of-dreams/self-serve/manual-recruitment-request/vacancy-details/grade")
     } else {
-        response.redirect("/voldemort/self-serve/manual-recruitment-request/vacancy-details/contract-type")
+        response.redirect("/field-of-dreams/self-serve/manual-recruitment-request/vacancy-details/contract-type")
     }
 })
 
-router.post('/voldemort/self-serve/manual-recruitment-request/pre-employment-checks/', function(request, response) {
+// Re-employment checks
+router.post('/field-of-dreams/self-serve/manual-recruitment-request/pre-employment-checks/', function(request, response) {
 
-    var liveInUK = request.session.data['mmrPreChecks']
-    if (liveInUK == "Yes"){
-        response.redirect("/voldemort/self-serve/manual-recruitment-request/pre-employment-checks/reserved-status")
+    var mmrPreChecks = request.session.data['mmrPreChecks']
+    if (mmrPreChecks == "Yes"){
+        response.redirect("/field-of-dreams/self-serve/manual-recruitment-request/pre-employment-checks/reserved-status")
     } else {
-        response.redirect("/voldemort/self-serve/manual-recruitment-request/check-your-answers")
+        response.redirect("/field-of-dreams/self-serve/manual-recruitment-request/check-your-answers")
     }
 })
 
